eff2fb2749b0cd935ad84d8800cf1c9bc6d6d6f5
cars0520 <cars0520@ISS2TTPKH2.scdsb.pri>
2020-09-16 12:19:08 -0400

    Finished part2 of review :D

---
+++ src/part2/PopulationGrowth.java
@@ -0,0 +1,43 @@
+
+package part2;
+
+import java.text.NumberFormat;
+
+
+public class PopulationGrowth {
+
+
+    public static void main(String[] args) {
+        /*
+        Barrie's pop in 2020 is 160000
+        grows by 6% every 10 years
+        How much will it be in 2120
+        */
+        int pop = 160000;
+        int growth;
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        //can alsoe use currency and %
+        
+        //print column headings
+        System.out.format("%10s", "YEAR");
+        System.out.format("%20s", "POPULATION");
+        System.out.format("20s", "GROWTH");
+        System.out.format("%20s\n", "NEW POPULATION");
+        
+        System.out.format("%5s", "====");
+        System.out.format("%20s", "==========");
+        System.out.format("%15s", "======");
+        System.out.format("%20s\n", "==============");
+        
+        for (int year = 2020; year <= 2120; year+=10) {
+            System.out.format("%5d", year);
+            System.out.format("%20s", nf.format(pop));
+            growth = (int)(.06 * pop); //have to cast integer
+            System.out.format("%15s", nf.format(growth));
+            pop += growth;
+            System.out.format("%20s\n", nf.format(pop));
+            
+        }
+    }
+    
+}
